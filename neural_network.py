e0791116be8a529d83fb654c8b9b8edbc112cb45
lucasdelimanogueira <47654903+lucasdelimanogueira@users.noreply.github.com>
2020-12-27 23:27:29 -0300

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,11 +1,6 @@
 import random
 import numpy as np
 
-### Técnicas utilizadas
-# Weight decay
-# Xavier Weight initialization
-# Cross-pEntropy Cost Function
-
 class MLP:
 
     def __init__(self, sizes):
@@ -14,23 +9,23 @@ class MLP:
         self.random_weight_biases()
 
     def random_weight_biases(self):
-        """Para uma lista [i,j,k,l] representando os layers, inicializa os biases jx1, kx1 e lx1 e os weights jxi e kxj e lxk"""
+        """To a [i,j,k,l] list representing the layers, initializes the biases matrix j x 1, k x 1 e l x 1 and weights matrix j x i e k x j e l x k"""
         self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
 
-        # inicialização dos pesos com média 0 e desvio padrão de 1/sqrt(n) para evitar problemas com saturação
+        # weights initialization with 0 mean and 1/sqrt(n) std deviation (Xavier weights initialization)
         self.weights = [np.random.randn(self.sizes[i], self.sizes[i - 1]) / np.sqrt(self.sizes[i - 1])
                         for i in range(1, len(self.sizes))]
 
     def sigmoid(self, z):
-        """Para um array z, retorna o sigmoid de cada elemento"""
+        """To an array z, return element-wise sigmoid function"""
         return 1.0/(1.0 + np.exp(-z))
 
     def sigmoid_derivative(self, z):
-        """Para um array z, retorna a derivada sigmoid de cada elemento"""
+        """To an array z, return element-wise sigmoid prime function"""
         return self.sigmoid(z)*(1-self.sigmoid(z))
 
     def feedforward(self, a):
-        """Para uma entrada a, no formato [[a1],[a2]...] retorna a saída [[y1],[y2],[y3]...]"""
+        """To an input a, with shape [[a1],[a2]...] return ANN feedforward output with shape [[y1],[y2],[y3]...]"""
 
         for w,b in zip(self.weights, self.biases):
             a = self.sigmoid(w @ a + b)
