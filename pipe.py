071f6777618724c32fe9be0e63a05454bf20351a
lucasdelimanogueira <47654903+lucasdelimanogueira@users.noreply.github.com>
2020-12-27 23:30:46 -0300

    Update pipe.py

--- pipe.py
+++ pipe.py
@@ -6,7 +6,7 @@ class Pipe:
     def __init__(self, x=250):
 
         self.PIPE_IMG = pygame.image.load(os.path.join('images', 'pipe.png'))
-        self.GAP = 100 # gap entre cano superior e inferior
+        self.GAP = 100 # gap between upper and lower pipes
         self.VEL = 5
 
         self.x = x
@@ -29,21 +29,21 @@ class Pipe:
         self.x -= self.VEL
 
     def draw(self, win):
-        win.blit(self.PIPE_TOP, (self.x, self.top)) # coloca o cano superior na tela
-        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom)) # coloca o cano inferior na tela
+        win.blit(self.PIPE_TOP, (self.x, self.top)) # draws the upper pipe on screen
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom)) # draws the lower pipe on screen
 
     def collide(self, bird):
         bird_mask = bird.get_mask()
         top_pipe_mask = pygame.mask.from_surface(self.PIPE_TOP)
         bottom_pipe_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
 
-        top_offset = (self.x - bird.x, self.top - round(bird.y)) # distancias (x,y) entre pássaro e top pipe
-        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y)) # distancias (x,y) entre pássaro e bottom pipe
+        top_offset = (self.x - bird.x, self.top - round(bird.y)) # distances (x,y) between bird and upper pipe
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y)) # distances (x,y) between bird and lower pipe
 
-        t_point = bird_mask.overlap(top_pipe_mask, top_offset) # ponto de colisão passáro-top_pipe
-        b_point = bird_mask.overlap(bottom_pipe_mask, bottom_offset) # ponto de colisão pássario-bottom_pipe
+        t_point = bird_mask.overlap(top_pipe_mask, top_offset) # collision point between bird and upper pipe
+        b_point = bird_mask.overlap(bottom_pipe_mask, bottom_offset) # collision point between bird and lower pipe
 
-        if t_point or b_point: # se existir um ponto de colisão
+        if t_point or b_point: # if exists a collision point
             return True
 
         return False
