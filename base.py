c94221955775b030f3337210250f749e292bd6bd
lucasdelimanogueira <47654903+lucasdelimanogueira@users.noreply.github.com>
2020-12-27 22:41:20 -0300

    Update base.py

--- base.py
+++ base.py
@@ -1,29 +1,29 @@
 import pygame
 import os
 
-class Base:
+class Ground:
     def __init__(self, y=365):
-        self.IMG = pygame.image.load(os.path.join('images', 'base.png'))
+        self.IMG = pygame.image.load(os.path.join('images', 'ground.png'))
         self.VEL = 5
         self.WIDTH = self.IMG.get_width()
 
         self.y = y
-        self.x1 = 0 # base 1
-        self.x2 = self.WIDTH # base 2
+        self.x1 = 0 # ground 1
+        self.x2 = self.WIDTH # ground 2
 
     def move(self):
 
-        # base 1 e base 2 enfileirados
+        # ground 1 and ground 2 in a row
 
-        self.x1 -= self.VEL # move base 1
-        self.x2 -= self.VEL # move base 2
+        self.x1 -= self.VEL # move ground 1
+        self.x2 -= self.VEL # move ground 2
 
-        if self.x1 + self.WIDTH < 0: # quando base 1 sair da tela, vai para trás d1 base 2
+        if self.x1 + self.WIDTH < 0: # when ground 1 leaves the screen, it goes behind ground 2
             self.x1 = self.x2 + self.WIDTH
 
-        if self.x2 + self.WIDTH < 0: # quando base 2 sair da tela, vai para trás de base 1
+        if self.x2 + self.WIDTH < 0: # when ground 2 leaves the screen, it goes behind ground 1
             self.x2 = self.x1 + self.WIDTH
 
     def draw(self, win):
-        win.blit(self.IMG, (self.x1, self.y)) # coloca base 1 na tela
-        win.blit(self.IMG, (self.x2, self.y)) # coloca base 2 na tela
+        win.blit(self.IMG, (self.x1, self.y)) # put ground 1 on the screen
+        win.blit(self.IMG, (self.x2, self.y)) # put ground 2 on the screen
